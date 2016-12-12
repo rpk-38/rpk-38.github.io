dd2e48a65ad60567de79b5ea08407900dfa5de00
rpk-38 <seriouszoxa@yandex.ru>
2016-12-12 18:09:09 +0800

    Initial commit

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php
+	
+?>
+<!DOCTYPE html>
+<html lang="ru-RU">
+	<head>
+		<meta charset="UTF-8">
+		<title>ООО "РЕГИОНАЛЬНАЯ ПРОДУКТОВАЯ КОМПАНИЯ"</title> 
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="wrapper">
+			<header>
+				<h1><a href="index.html"><img src="img/logo.png" alt="logo"></a></h1> <!-- Поменять лого ОБЯЗАТЕЛЬНО -->
+				<span class="tel">123-456-7890</span>
+				<nav>
+					<ul>
+						<li><a href="contact.php" title="Обратная связь">Обратная связь</a></li>	<!-- Обратная связь-->
+						<li><a href="magazine.html" title="Магазины">Магазины</a></li>  <!-- Все магазины куда поставляется продукция -->
+						<li><a href="about.html" title="О Нас">О Нас</a></li>	<!-- О нас -->
+						<li><a href="index.html" title="Главную">Главную</a></li>	<!-- На главную -->
+					</ul>
+				</nav>
+			</header>
+			<div class="page-block">
+				<p></p>
+				<h1>Свяжитесь с нами</h1>
+			</div>
+			
+			<hr>
+			<footer>
+				<ul class="top-footer">
+					<p>© 2016, Все права защищены.</p><br>
+					<p>Перепечатка материалов сайта - только с разрешения администрации сайта.</p>
+				</ul>
+			</footer>
+		</div>
+	</body>
+</html>
